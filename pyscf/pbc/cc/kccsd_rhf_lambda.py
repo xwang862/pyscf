47f10856b1676656748063d04f3cbf508f96cd77
Xiao Wang <xiaowang862@gmail.com>
2020-07-05 19:39:29 -0400

    Init KRCCSD lambda solver

---
+++ pyscf/pbc/cc/kccsd_rhf_lambda.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# Copyright 2014-2020 The PySCF Developers. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Xiao Wang <xiaowang314159@gmail.com>
+#
+
+'''
+Lambda equations for spin-restricted KCCSD.
+'''
+import time
+import numpy
+from pyscf import lib
+from pyscf.cc import ccsd_lambda
+from pyscf.lib import logger
+from pyscf.pbc.mp.kmp2 import padding_k_idx
+from pyscf.lib.parameters import LARGE_DENOM
+from pyscf.pbc.lib import kpts_helper
+
+einsum = lib.einsum
+
+def kernel(cc, eris=None, t1=None, t2=None, l1=None, l2=None, max_cycle=50, tol=1e-8, verbose=logger.INFO):
+    log = logger.Logger(cc.stdout, cc.verbose)
+    log.info("******** PBC RCCSD lambda solver ********")
+    return ccsd_lambda.kernel(cc, eris, t1, t2, l1, l2, max_cycle=max_cycle, tol=tol, verbose=verbose, fintermediates=make_intermediates, fupdate=update_lambda)
+
+def make_intermediates(cc, t1=None, t2=None, eris=None):
+    from pyscf.pbc.cc.eom_kccsd_rhf import _IMDS
+    if eris is None: eris = cc.ao2mo(cc.mo_coeff)
+    imds = _IMDS(cc, eris)
+    imds.make_ee()
+    return imds
+
+def update_lambda(cc, t1, t2, l1, l2, eris, imds):
+    return 0, 0
+    
